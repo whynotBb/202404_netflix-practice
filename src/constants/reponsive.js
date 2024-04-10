fb527557369ea56b47cfaec51f30638e8b0f4bbf
kimsese <whynot.ksw@gmail.com>
2024-04-10 13:43:00 +0900

    반응형 수정

--- src/constants/reponsive.js
+++ src/constants/reponsive.js
@@ -1,16 +1,21 @@
 export const responsive = {
     desktop: {
-        breakpoint: { max: 3000, min: 1024 },
+        breakpoint: { max: 3000, min: 1600 },
         items: 5,
         slidesToSlide: 3, // optional, default to 1.
     },
+    tabletLg: {
+        breakpoint: { max: 1600, min: 1024 },
+        items: 4,
+        slidesToSlide: 2, // optional, default to 1.
+    },
     tablet: {
-        breakpoint: { max: 1024, min: 464 },
+        breakpoint: { max: 1024, min: 760 },
         items: 2,
         slidesToSlide: 2, // optional, default to 1.
     },
     mobile: {
-        breakpoint: { max: 464, min: 0 },
+        breakpoint: { max: 760, min: 0 },
         items: 1,
         slidesToSlide: 1, // optional, default to 1.
     },
